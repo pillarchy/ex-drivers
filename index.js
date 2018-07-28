c7640f75cff85c1a7275e3e16c41146300c618b5
Chunlong <longbill.cn@gmail.com>
2018-07-28 15:52:26 +0800

    add bitstamp ws

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
 	OKEX_FUTURE: require('./exchanges/okex_future/index.js'),
 	BINANCE: require('./exchanges/binance/index.js'),
 	BITFINEX: require('./exchanges/bitfinex/index.js'),
+	BITSTAMP: require('./exchanges/bitstamp/index.js'),
 	BITFLYER_FX: require('./exchanges/bitflyer_fx/index.js'),
 	HUOBI: require('./exchanges/huobi/index.js'),
 	HADAX: require('./exchanges/huobi/hadax.js'),
